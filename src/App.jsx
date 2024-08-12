f0ff2974fb362f6d0e3cf6f0308d020cc167771e
Silvio <nirvash533@gmail.com>
2024-08-12 16:34:04 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+
+    <div>
+      <h1> This is a practice website</h1>
+    </div>
+  )
+}
+
+export default App
