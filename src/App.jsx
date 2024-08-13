9f4b539b35e9666c70c012326a2294f42d915896
Silvio <nirvash533@gmail.com>
2024-08-13 14:49:26 -0500

    Added a new example paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   //We can create a variable that will hold the values of the style we want to apply to our HTML.
-  const customStyle ={
-    color : "red",
-    fontSize : "20px",
-    border: "1px solid black"
-  }
-
+  const customStyle = {
+    color: "red",
+    fontSize: "20px",
+    border: "1px solid black",
+  };
 
   return (
-//Here we add the variable into our H1
+    //Here we add the variable into our H1
     <div>
-      <h1 style={customStyle} > This is a practice website</h1>
+      <h1 style={customStyle}> This is a practice website</h1>
+      <h1 style={{ color: "green", fontSize: "30px", border: "1px solid black" }}>el precio de la yuca
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
